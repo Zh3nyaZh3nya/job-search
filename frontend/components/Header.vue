8d13c1b510c242b33e3975ac63c62180d428bb7f
Zh3nyaZh3nya <evgeniq.golovkin@gmail.com>
2024-12-07 20:02:45 +0500

    added hero & cards

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -37,10 +37,10 @@ const changeLanguage = (lang: LocaleCode): void => {
 </script>
 
 <template>
-  <v-toolbar color="transparent" class="py-2">
+  <v-app-bar class="bg-transparent py-2 text-white position-absolute" elevation="0">
     <v-container class="d-flex">
       <nuxt-link :to="localePath('/')" class="mr-10 text-h4">
-        <span class="text-primary font-weight-bold">Search</span><span class="text-body">Job</span>
+        <span class="text-primary font-weight-bold">Search</span><span>Job</span>
       </nuxt-link>
       <ul class="d-flex align-center ga-4">
         <li v-for="item in menu" :key="item.link" class="text-h6">
@@ -52,18 +52,22 @@ const changeLanguage = (lang: LocaleCode): void => {
       <v-spacer />
       <v-btn
           variant="outlined"
-          class="text-none text-h6 mr-4"
+          class="text-none text-body-1 mr-4"
           rounded="xl"
           elevation="0"
           @click="changeLanguage(locale === 'ru' ? locales[1].code : locales[0].code)"
       >
         {{ locale === 'ru' ? locales[1].name : locales[0].name }}
       </v-btn>
-      <v-btn variant="tonal" color="primary" class="text-none text-h6" rounded="xl" elevation="0">
+      <v-btn
+          class="bg-primary text-none text-body-1"
+          rounded="xl"
+          elevation="0"
+      >
         {{ $t('enter') }}
       </v-btn>
     </v-container>
-  </v-toolbar>
+  </v-app-bar>
 </template>
 
 <style scoped>
---
+++ frontend/components/Search.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from "vue"
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+
+const selectItem = ref('job')
+const selectItems = ref([
+  {
+    title: t('search_selects_1'),
+    value: 'job',
+  },
+  {
+    title: t('search_selects_2'),
+    value: 'members'
+  }
+])
+</script>
+
+<template>
+  <v-sheet color="white" rounded="pill" width="100%" min-height="74px" elevation="12" class="d-flex align-center pa-3">
+    <v-form class="d-flex align-center w-100 ga-2">
+      <v-select
+          v-model="selectItem"
+          :items="selectItems"
+          rounded="pill"
+          size="x-large"
+          variant="solo"
+          :hide-details="true"
+          min-width="220"
+      ></v-select>
+
+      <v-text-field :placeholder="$t('search_placeholder')" :hide-details="true" variant="solo" elevation="0" width="100%" rounded="pill">
+
+      </v-text-field>
+      <v-btn rounded="pill" class="text-none text-body-1" size="x-large" height="56px" color="primary">
+        {{ $t('search') }}
+      </v-btn>
+    </v-form>
+  </v-sheet>
+</template>
+
+<style scoped>
+
+</style>
