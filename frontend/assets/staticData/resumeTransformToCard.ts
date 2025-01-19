42144271c4263c495b739b8bc86730ecaaa6a2c7
Zh3nyaZh3nya <evgeniq.golovkin@gmail.com>
2025-01-20 02:45:17 +0500

    fixed ts errors

--- frontend/assets/staticData/resumeTransformToCard.ts
+++ frontend/assets/staticData/resumeTransformToCard.ts
@@ -2,10 +2,19 @@ import type { IResume, IResumeCard, IResumeCardSmall } from "~/types";
 
 export function resumeTransformToCard(
     resumes: IResume[],
+    type: "small"
+): IResumeCardSmall[];
+export function resumeTransformToCard(
+    resumes: IResume[],
+    type: "usually"
+): IResumeCard[];
+
+export function resumeTransformToCard(
+    resumes: IResume[],
     type: "small" | "usually"
 ): IResumeCardSmall[] | IResumeCard[] {
-    return resumes.map((resume: IResume): IResumeCardSmall | IResumeCard => {
-        const baseCard = {
+    if (type === "small") {
+        return resumes.map((resume: IResume) => ({
             id: resume.id,
             slug: resume.slug,
             title: resume.title,
@@ -14,18 +23,21 @@ export function resumeTransformToCard(
             work_experience: resume.work_experience,
             avatar: resume.avatar,
             education_level: resume.education_level,
-        };
-
-        if (type === "small") {
-            return baseCard as IResumeCardSmall;
-        } else {
-            return {
-                ...baseCard,
-                category: resume.category,
-                date_publish: resume.date_publish,
-                city: resume.info_resume.city,
-                to_salary: resume.to_salary
-            } as IResumeCard;
-        }
-    }).slice(0, type === "small" ? 4 : resumes.length);
+        })).slice(0, 4) as IResumeCardSmall[];
+    } else {
+        return resumes.map((resume: IResume) => ({
+            id: resume.id,
+            slug: resume.slug,
+            title: resume.title,
+            from_salary: resume.from_salary,
+            currency: resume.currency,
+            work_experience: resume.work_experience,
+            avatar: resume.avatar,
+            education_level: resume.education_level,
+            category: resume.category,
+            date_publish: resume.date_publish,
+            city: resume.info_resume.city,
+            to_salary: resume.to_salary,
+        })) as IResumeCard[];
+    }
 }
